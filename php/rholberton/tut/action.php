73c9aafb9384396ecd55231b6efda25b11d13425
rholberton <rhonda_holberton7@hotmail.com>
2020-03-28 12:08:55 -0700

    REMOVED FOR TUTORIAL

--- php/rholberton/tut/action.php
+++
@@ -1,9 +0,0 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- Hi <?php echo htmlspecialchars($_POST['name']); ?>.
- You are <?php echo (int)$_POST['age']; ?> years old.  
- </body>
-</html>
--- php/rholberton/tut/actionget.php
+++
@@ -1,9 +0,0 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- Hi <?php echo htmlspecialchars($_GET['name']); ?>.
- You are <?php echo (int)$_GET['age']; ?> years old.  
- </body>
-</html>
